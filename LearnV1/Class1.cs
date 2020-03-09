9dcdbfc31a49a52dc273004cf8906b1eab285b3e
Jeffries Mamani <mamani@mmsi.com>
2020-03-09 13:34:55 -0300

    Programa base

---
+++ LearnV1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnV1
+{
+    public class rectangle
+    {
+        public double basis;
+        public double height;
+
+        public double area()
+        {
+            double area = this.basis * this.height;
+            return area;
+        }
+
+        public double perimeter()
+        {
+            double perim = this.basis * 2 + this.height * 2;
+            return perim;
+        }
+
+        public void grow(double quantity)
+        {
+            //NO RETURNO NADA PORQUE ES VOID, SOLO CAMBIA EL VALOR ACTUAL DE LAS VARIABLES
+            this.basis = this.basis + quantity;
+            this.height = this.height + quantity;
+        }
+
+        public double compare(rectangle rect)
+        {
+            return this.area() - rect.area();
+        }
+
+
+
+    }
+}
--- LearnV1/Program.cs
+++ LearnV1/Program.cs
@@ -6,8 +6,34 @@ namespace LearnV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //v1
+            //Console.WriteLine("Hello World!");
+
+            rectangle rectangle1 = new rectangle();
+            rectangle1.basis = 3;
+            rectangle1.height = 7;
+
+            Console.WriteLine(rectangle1.area());
+            Console.WriteLine(rectangle1.perimeter());
+
+            //rectangle1.grow(3); //suma 3 a basis y height
+
+            Console.WriteLine(rectangle1.area());
+            Console.WriteLine(rectangle1.perimeter());
+
+            rectangle rectangle2 = new rectangle();
+            rectangle2.basis = 1.5;
+            rectangle2.height = 4;
+
+            //diferencia entre area rectangle1 y rectangle1
+            Console.WriteLine(rectangle1.compare(rectangle2));
+
+            rectangle rectangle3 = new rectangle();
+            rectangle3.basis = 8;
+            rectangle3.height = 12;
+
+            //obs
+
+            Console.ReadKey();
         }
     }
 }
